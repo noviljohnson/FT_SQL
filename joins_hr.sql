6adf8d06519c7a1119f352c47b47c7e4b9c20037
Novil Johnson <noviljohnson9@gmail.com>
2022-12-29 17:31:42 +0530

    joins hr schma

---
+++ joins_hr.sql
@@ -0,0 +1,49 @@
+-- Active: 1671700995648@@127.0.0.1@3308@hr
+
+
+--  Joins
+
+SELECT r.region_name, c.country_name from regions r INNER JOIN countries c on r.region_id=c.region_id;
+
+desc regions;
+desc countries;
+desc locations;
+
+SELECT r.region_name,l.country_id,l.city,c.country_name from locations l INNER JOIN countries c on c.country_id=l.country_id INNER JOIN regions r on r.region_id =c.region_id;
+
+SHOW TABLES;
+
+SELECT d.department_id,d.location_id,d.department_name from departments d INNER JOIN locations l on d.location_id=l.location_id;
+
+SELECT r.region_name,c.country_name,l.city,d.department_name,CONCAT(e.first_name," ",e.last_name) "name" 
+from regions r INNER JOIN countries c
+on r.region_id=c.region_id
+JOIN locations l on l.country_id=c.country_id
+JOIN departments d on d.location_id=l.location_id
+JOIN employees e on d.department_id=e.department_id;
+desc employees;
+
+
+-- self joins;
+
+SELECT e.first_name,e.last_name,m.first_name from employees e INNER JOIN employees m on e.manager_id=m.employee_id ORDER BY e.last_name;
+SELECT e.first_name,e.last_name,m.first_name from employees e LEFT OUTER JOIN employees m on e.manager_id=m.employee_id ORDER BY m.first_name;
+
+
+-- SELECT e.first_name,m.first_name,m.salary,e.salary from employees e JOIN employees m on e.salary=m.salary and e.employee_id != m.employee_id ;
+
+SELECT CONCAT(first_name,last_name), salary from employees
+WHERE salary in(SELECT salary from employees
+                GROUP BY salary
+                HAVING COUNT(employee_id) > 1);
+
+
+SELECT * FROM employees;
+
+
+
+-- find job_id which got filled in 2nd half of any year again filled in the 1st half of next YEAR
+
+
+SELECT job_id,YEAR(e.hire_date),QUARTER(e.hire_date),e.first_name from employees e WHERE QUARTER(hire_date) > 2 and job_id in (
+        SELECT job_id FROM employees m WHERE QUARTER(m.hire_date) < 3 and YEAR(m.hire_date) = YEAR(e.hire_date)+1 );
---
+++ set_ops.sql
@@ -0,0 +1,135 @@
+-- Active: 1671700995648@@127.0.0.1@3308@ft_db
+
+-- set operators
+
+-- Union
+-- Union ALL
+-- Intersection (8.0.31 version)
+
+SELECT distinct(job) from emp WHERE `DEPTNO` in (10,20);
+
+SELECT job from emp WHERE deptno=10 
+UNION
+SELECT job from emp WHERE `DEPTNO`=20;
+
+SELECT job from emp WHERE deptno=10 
+UNION ALL
+SELECT job from emp WHERE `DEPTNO`=20;
+
+SELECT job from emp WHERE deptno=10 
+INTERSECT
+SELECT job from emp WHERE `DEPTNO`=20;
+
+SELECT job from emp WHERE deptno=10 
+INTERSECT
+SELECT job from emp WHERE `DEPTNO`=20
+INTERSECT
+SELECT job from emp WHERE deptno=30 ;
+
+-- find out total salary for each job as well total salary
+SELECT sum(sal) from emp GROUP BY job 
+UNION
+SELECT sum(sal) from emp;
+
+SELECT sum(sal),`JOB` from emp GROUP BY job 
+UNION
+SELECT sum(sal),NULL from emp;
+
+SELECT ename, null "dname" from emp
+UNION
+SELECT null, dname from dept;
+
+
+SELECT sum(sal),job
+from emp
+GROUP BY job with ROLLUP;
+
+SELECT sum(sal),job,`DEPTNO`
+from emp
+GROUP BY job,`DEPTNO` with ROLLUP;
+
+SELECT sum(sal),`DEPTNO`,`JOB`
+from emp
+GROUP BY `DEPTNO`,`JOB` with ROLLUP;
+
+SELECT sum(sal),`JOB`,`DEPTNO`
+from emp
+GROUP BY `JOB`,`DEPTNO` WITH ROLLUP;
+
+-- CUBE
+
+SELECT sum(sal),deptno, job
+from emp
+GROUP BY `DEPTNO`,`JOB` WITH ROLLUP
+UNION
+SELECT sum(sal), `DEPTNO`,`JOB`
+from emp
+GROUP BY job, `DEPTNO` WITH ROLLUP;
+
+
+SELECT ename,null "dname" from emp
+UNION
+SELECT null, dname from dept
+ORDER BY dname;
+
+SELECT sum(sal), deptno,job
+from emp
+GROUP BY `DEPTNO`,job with ROLLUP
+UNION
+SELECT sum(sal),`DEPTNO`,`JOB`
+from emp
+GROUP BY job,`DEPTNO` WITH ROLLUP;
+
+
+
+-- JOINS
+
+SELECT e.ename, d.dname from emp e INNER JOIN dept d ON e.`DEPTNO`=d.`DEPTNO`;
+
+SELECT e.ename, d.dname from emp e INNER JOIN dept d ON e.`DEPTNO`=d.`DEPTNO` and d.`DNAME`="SALES";
+SELECT e.ename, d.dname from emp e INNER JOIN dept d ON e.`DEPTNO`=d.`DEPTNO` WHERE d.`DNAME` = "Sales";
+SELECT e.ename, d.dname from emp e INNER JOIN dept d ON e.`DEPTNO`=d.`DEPTNO` WHERE d.`DNAME` = "sales" ORDER BY e.`ENAME`;
+
+-- find out total salary in sales, accounting department
+
+SELECT SUM(e.sal), d.`DNAME` from emp e INNER JOIN dept d ON e.`DEPTNO`=d.`DEPTNO` WHERE d.`DNAME` in("accounting","sales")  GROUP BY d.`DNAME`;
+
+SELECT e.ename,e.job,d.`LOC` from emp e INNER JOIN dept d on e.`DEPTNO`=d.`DEPTNO` WHERE d.`LOC`='Chicago'and e.`JOB`='clerk';
+
+SELECT * from emp e INNER JOIN dept d on e.`DEPTNO`=d.`DEPTNO`;
+
+
+-- outer JOIN
+
+SELECT e.ename, e.job, d.loc from emp e right OUTER JOIN dept d on e.`DEPTNO` = d.`DEPTNO`;
+
+SELECT e.ename, d.dname from emp e RIGHT OUTER JOIN dept d on e.`DEPTNO`=d.`DEPTNO` 
+UNION
+SELECT e.ename,d.`DNAME` FROM emp e LEFT OUTER JOIN dept d on e.`DEPTNO`=d.`DEPTNO`;
+
+
+-- non equi JOIN
+SELECT e.ename,s.grade from emp e join salgrade s on e.sal BETWEEN s.`LOSAL` and s.`HISAL`;
+
+SELECT e.ename,d.dname,s.grade from dept d INNER JOIN emp e on e.`DEPTNO`=d.`DEPTNO` INNER JOIN salgrade s on e.sal BETWEEN s.`LOSAL` and s.`HISAL`;
+
+
+create table job_grades
+(grade_level char(1),
+ lowest_sal numeric(11,2),
+ high_sal numeric(11,2));
+
+insert into job_grades
+values ("A",1000,2999),
+("B",3000,5999),
+("C",6000,9999),
+ ("D",10000,14999),
+ ("E",15000,24999),
+ ("F",25000,40000);
+
+
+
+SELECT e.job,e.`ENAME` from emp e WHERE QUARTER(hiredate) >2 and job in (
+        SELECT m.job FROM emp m WHERE QUARTER(m.hiredate) < 3 and YEAR(m.hiredate) = YEAR(e.hiredate)+1 );
+SELECT * from emp;
+SELECT e.job,e.`ENAME`,YEAR(`HIREDATE`) from emp e WHERE QUARTER(hiredate) > 2;
